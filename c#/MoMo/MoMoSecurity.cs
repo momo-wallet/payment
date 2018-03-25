2f6262c05f8d05a569f0195917d006dba86b838d
KhangDC <caokhang91@gmail.com>
2018-03-25 23:08:52 +0700

    add new c# project

---
+++ c#/MoMo/MoMoSecurity.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MoMo
+{
+    class MoMoSecurity
+    {
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public MoMoSecurity()
+        {
+            //encrypt and decrypt password using secure
+        }
+        public string getHash(string partnerCode, string merchantRefId,
+            string amount, string paymentCode, string storeId, string storeName, string publicKey)
+        {
+            string json = "{\"partnerCode\":\"" + 
+                partnerCode + "\",\"partnerRefId\":\"" +
+                merchantRefId + "\",\"amount\":" +
+                amount + ",\"paymentCode\":\"" +
+                paymentCode + "\",\"storeId\":\"" +
+                storeId + "\",\"storeName\":\"" +
+                storeName + "\"}";
+            log.Debug("Raw hash: " + json);
+            byte[] data = Encoding.UTF8.GetBytes(json);
+            string result = null;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    // client encrypting data with public key issued by server
+                    rsa.FromXmlString(publicKey);
+                    var encryptedData = rsa.Encrypt(data, false);
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+                    result = base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+
+            }
+
+            return result;
+
+        }
+        public string buildQueryHash(string partnerCode, string merchantRefId,
+            string requestid, string publicKey)
+        {
+            string json = "{\"partnerCode\":\"" +
+                partnerCode + "\",\"partnerRefId\":\"" +
+                merchantRefId + "\",\"requestId\":\"" +
+                requestid + "\"}";
+            log.Debug("Raw hash: " + json);
+            byte[] data = Encoding.UTF8.GetBytes(json);
+            string result = null;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    // client encrypting data with public key issued by server
+                    rsa.FromXmlString(publicKey);
+                    var encryptedData = rsa.Encrypt(data, false);
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+                    result = base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+
+            }
+
+            return result;
+
+        }
+
+        public string buildRefundHash(string partnerCode, string merchantRefId,
+            string momoTranId, long amount, string description, string publicKey)
+        {
+            string json = "{\"partnerCode\":\"" +
+                partnerCode + "\",\"partnerRefId\":\"" +
+                merchantRefId + "\",\"momoTransId\":\"" +
+                momoTranId + "\",\"amount\":" +
+                amount + ",\"description\":\"" +
+                description + "\"}";
+            log.Debug("Raw hash: " + json);
+            byte[] data = Encoding.UTF8.GetBytes(json);
+            string result = null;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+                    // client encrypting data with public key issued by server
+                    rsa.FromXmlString(publicKey);
+                    var encryptedData = rsa.Encrypt(data, false);
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+                    result = base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+
+            }
+
+            return result;
+
+        }
+        public string signSHA256(string message, string key)
+        {
+            byte[] keyByte = Encoding.UTF8.GetBytes(key);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            using (var hmacsha256 = new HMACSHA256(keyByte))
+            {
+                byte[] hashmessage = hmacsha256.ComputeHash(messageBytes);
+                string hex = BitConverter.ToString(hashmessage);
+                hex = hex.Replace("-", "").ToLower();
+                return hex;
+
+            }
+        }
+    }
+}
