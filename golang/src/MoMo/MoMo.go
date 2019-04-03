ac865d7fbb0968a350875bebb116c50014338c58
Doan Cao Khang <khangdoan@CNTT-KhangDoan.local>
2019-04-03 18:24:34 +0700

    add goland to familly

---
+++ golang/src/MoMo/MoMo.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	//run for download library `go get github.com/sony/sonyflake`
+	"github.com/sony/sonyflake"
+)
+
+//define a payload, refference in https://developers.momo.vn/#cong-thanh-toan-momo-phuong-thuc-thanh-toan
+type Payload struct {
+	PartnerCode string `json:"partnerCode"`
+	AccessKey   string `json:"accessKey"`
+	RequestID   string `json:"requestId"`
+	Amount      string `json:"amount"`
+	OrderID     string `json:"orderId"`
+	OrderInfo   string `json:"orderInfo"`
+	ReturnURL   string `json:"returnUrl"`
+	NotifyURL   string `json:"notifyUrl"`
+	ExtraData   string `json:"extraData"`
+	RequestType string `json:"requestType"`
+	Signature   string `json:"signature"`
+}
+
+func main() {
+	fmt.Printf("Hello Momo!\n")
+	flake := sonyflake.NewSonyflake(sonyflake.Settings{})
+
+	//randome orderID and requestID
+	a, err := flake.NextID()
+	b, err := flake.NextID()
+
+	var orderId = strconv.FormatUint(a, 16)
+	var requestId = strconv.FormatUint(b, 16)
+	var endpoint = "https://test-payment.momo.vn/gw_payment/transactionProcessor"
+	var partnerCode = "MOMOIQA420180417"
+	var accessKey = "SvDmj2cOTYZmQQ3H"
+	var serectkey = "PPuDXq1KowPT1ftR8DvlQTHhC03aul17"
+	var orderInfo = "momo all-in-one"
+	var returnUrl = "https://developers.momo.vn/"
+	var notifyurl = "https://webhook.site/3c5b6488-a159-4f8d-b038-29eed82fab1e"
+	var amount = "1000"
+	var requestType = "captureMoMoWallet"
+	var extraData = "merchantName=;merchantId=" //pass empty value if your merchant does not have stores else merchantName=[storeName]; merchantId=[storeId] to identify a transaction map with a physical store
+
+	//build raw signature
+	var rawSignature bytes.Buffer
+	rawSignature.WriteString("partnerCode=")
+	rawSignature.WriteString(partnerCode)
+	rawSignature.WriteString("&accessKey=")
+	rawSignature.WriteString(accessKey)
+	rawSignature.WriteString("&requestId=")
+	rawSignature.WriteString(requestId)
+	rawSignature.WriteString("&amount=")
+	rawSignature.WriteString(amount)
+	rawSignature.WriteString("&orderId=")
+	rawSignature.WriteString(orderId)
+	rawSignature.WriteString("&orderInfo=")
+	rawSignature.WriteString(orderInfo)
+	rawSignature.WriteString("&returnUrl=")
+	rawSignature.WriteString(returnUrl)
+	rawSignature.WriteString("&notifyUrl=")
+	rawSignature.WriteString(notifyurl)
+	rawSignature.WriteString("&extraData=")
+	rawSignature.WriteString(extraData)
+
+	// Create a new HMAC by defining the hash type and the key (as byte array)
+	hmac := hmac.New(sha256.New, []byte(serectkey))
+
+	// Write Data to it
+	hmac.Write(rawSignature.Bytes())
+	fmt.Println("Raw signature: " + rawSignature.String())
+
+	// Get result and encode as hexadecimal string
+	signature := hex.EncodeToString(hmac.Sum(nil))
+
+	var payload = Payload{
+		PartnerCode: partnerCode,
+		AccessKey:   accessKey,
+		RequestID:   requestId,
+		Amount:      amount,
+		OrderID:     orderId,
+		OrderInfo:   orderInfo,
+		ReturnURL:   returnUrl,
+		NotifyURL:   notifyurl,
+		ExtraData:   extraData,
+		RequestType: requestType,
+		Signature:   signature,
+	}
+
+	var jsonPayload []byte
+	jsonPayload, err = json.Marshal(payload)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("Payload: " + string(jsonPayload))
+	fmt.Println("Signature: " + signature)
+
+	//send HTTP to momo endpoint
+	resp, err := http.Post(endpoint, "application/json", bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//result
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)
+	fmt.Println(result)
+
+	//PayUrl
+	fmt.Println(result["payUrl"])
+
+}
