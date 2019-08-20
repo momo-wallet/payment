2947fffda0974ac7719c8d254d34a47aef1d07ec
Minh Hai <hai.nguyen@mservice.com.vn>
2019-08-20 11:15:48 +0700

    remove php, java example

--- java/paygate/src/main/java/com/mservice/paygate/model/PaymentRequest.java
+++
@@ -1,7 +0,0 @@
-package com.mservice.paygate.model;
-
-public class PaymentRequest extends Request {
-    public PaymentRequest(String partnerCode, String orderId, String orderInfo, String accessKey, String amount, String signature, String extraData, String requestId, String notifyUrl, String returnUrl, String requestType) {
-        super(partnerCode, orderId, orderInfo, accessKey, amount, signature, extraData, requestId, notifyUrl, returnUrl, requestType);
-    }
-}
