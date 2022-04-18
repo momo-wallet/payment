1f3e64d2a51aab34234420002571856bb07e2d53
Le Sang <sang.le@mservice.com.vn>
2022-04-18 15:37:10 +0700

    upgrade python2 to python3

--- python/MoMo.py
+++ python/MoMo.py
@@ -1,11 +1,12 @@
 import json
-import urllib2
+import urllib.request
+import urllib
 import uuid
+import requests
 import hmac
 import hashlib
 
-
-#parameters send to MoMo get get payUrl
+# parameters send to MoMo get get payUrl
 endpoint = "https://test-payment.momo.vn/v2/gateway/api/create"
 partnerCode = "MOMO"
 accessKey = "F8BBA842ECF85"
@@ -17,56 +18,48 @@ amount = "50000"
 orderId = str(uuid.uuid4())
 requestId = str(uuid.uuid4())
 requestType = "captureWallet"
-extraData = "" #pass empty value or Encode base64 JsonString
+extraData = ""  # pass empty value or Encode base64 JsonString
 
-#before sign HMAC SHA256 with format: accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
-rawSignature = "accessKey="+accessKey+"&amount="+amount+"&extraData="+extraData+"&ipnUrl="+ipnUrl+"&orderId="+orderId+"&orderInfo="+orderInfo+"&partnerCode="+partnerCode+"&redirectUrl="+redirectUrl+"&requestId="+requestId+"&requestType="+requestType
+# before sign HMAC SHA256 with format: accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl
+# &orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId
+# &requestType=$requestType
+rawSignature = "accessKey=" + accessKey + "&amount=" + amount + "&extraData=" + extraData + "&ipnUrl=" + ipnUrl + "&orderId=" + orderId + "&orderInfo=" + orderInfo + "&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl + "&requestId=" + requestId + "&requestType=" + requestType
 
-#puts raw signature
-print "--------------------RAW SIGNATURE----------------"
-print rawSignature
-#signature
-h = hmac.new( secretKey, rawSignature, hashlib.sha256 )
+# puts raw signature
+print("--------------------RAW SIGNATURE----------------")
+print(rawSignature)
+# signature
+h = hmac.new(bytes(secretKey, 'ascii'), bytes(rawSignature, 'ascii'), hashlib.sha256)
 signature = h.hexdigest()
-print "--------------------SIGNATURE----------------"
-print signature
+print("--------------------SIGNATURE----------------")
+print(signature)
 
-#json object send to MoMo endpoint
+# json object send to MoMo endpoint
 
 data = {
-        'partnerCode' : partnerCode,
-      	'partnerName' : "Test",
-      	'storeId' : "MomoTestStore",
-      	'requestId' : requestId,
-      	'amount' : amount,
-      	'orderId' : orderId,
-      	'orderInfo' : orderInfo,
-      	'redirectUrl' : redirectUrl,
-      	'ipnUrl' : ipnUrl,
-      	'lang' : "vi",
-      	'extraData' : extraData,
-      	'requestType' : requestType,
-      	'signature' : signature
+    'partnerCode': partnerCode,
+    'partnerName': "Test",
+    'storeId': "MomoTestStore",
+    'requestId': requestId,
+    'amount': amount,
+    'orderId': orderId,
+    'orderInfo': orderInfo,
+    'redirectUrl': redirectUrl,
+    'ipnUrl': ipnUrl,
+    'lang': "vi",
+    'extraData': extraData,
+    'requestType': requestType,
+    'signature': signature
 }
-print "--------------------JSON REQUEST----------------\n"
+print("--------------------JSON REQUEST----------------\n")
 data = json.dumps(data)
-print data
+print(data)
 
 clen = len(data)
-req = urllib2.Request(endpoint, data, {'Content-Type': 'application/json', 'Content-Length': clen})
-f = urllib2.urlopen(req)
+response = requests.post(endpoint, data=data, headers={'Content-Type': 'application/json', 'Content-Length': str(clen)})
 
-response = f.read()
-f.close()
-print "--------------------JSON response----------------\n"
-print response+"\n"
+# f.close()
+print("--------------------JSON response----------------\n")
+print(response.json())
 
-print "payUrl\n"
-print json.loads(response)['payUrl']+"\n"
-
-
-
-
-
-
-
+print(response.json()['payUrl'])
